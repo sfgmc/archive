c484e64ddb66eeb7c882dd161819ffa9862de2f8
beegeiger <beegeiger@gmail.com>
2019-09-24 13:29:58 -0700

    With API Token, Map now appears with Marker

--- client/src/components/DisplayEntry/LocationDisplay.js
+++ client/src/components/DisplayEntry/LocationDisplay.js
@@ -2,16 +2,17 @@ import GoogleMapReact from 'google-map-react';
 import { Block } from 'jsxstyle';
 import React from 'react';
 import { Pin } from '../Pin';
-import { googleMapsAPItoken } from './.env';
+
 export const LocationDisplay = ({ entry }) => {
-  console.log('LocationDisplay render', entry);
-  if (entry.contentType.sys.id !== 'locations') {
+console.log('LocationDisplay render', entry);
+
+  if (entry.contentType !== 'locations') {
     return null;
   }
   return (
     <Block width="100%" height={300}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: googleMapsAPItoken }}
+        bootstrapURLKeys={{ key:  }}
         defaultCenter={{
           lat: entry.geolocation.lat,
           lng: entry.geolocation.lon
