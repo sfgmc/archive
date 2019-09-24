a50e7bbd3f1f5b2f004dca18a8f5467205948506
beegeiger <beegeiger@gmail.com>
2019-09-24 13:34:16 -0700

    Added placeholder for API token so development server will load

--- client/src/components/DisplayEntry/LocationDisplay.js
+++ client/src/components/DisplayEntry/LocationDisplay.js
@@ -12,7 +12,7 @@ console.log('LocationDisplay render', entry);
   return (
     <Block width="100%" height={300}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key:  }}
+        bootstrapURLKeys={{ key: "123" }}
         defaultCenter={{
           lat: entry.geolocation.lat,
           lng: entry.geolocation.lon
